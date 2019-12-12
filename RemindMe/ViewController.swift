ec56ff04d64ca082fe17c39ec632e675efc617a8
Nicolas TOURRETTE <nicolas_tourrette@etu.u-bourgogne.fr>
2019-12-12 12:15:31 +0100

    Start developping UIg

---
+++ RemindMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemindMe
+//
+//  Created by Nicolas TOURRETTE on 12/12/2019.
+//  Copyright © 2019 Nicolas TOURRETTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
